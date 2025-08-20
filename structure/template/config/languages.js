a75bf01284259ac903b0b88bcc93f49b34897ab4
annelvk <23258766+annelvk@users.noreply.github.com>
2025-08-20 18:02:04 +0200

    default languages

--- structure/template/config/languages.js
+++ structure/template/config/languages.js
@@ -1,5 +1,5 @@
 // 1. Define languages for the application
-const supportedLangs = ['fr', 'en', 'de'];
+const supportedLangs = ['fr-FR', 'en-EN'];
 
 // 2. Overload directory to use if any
 // Expected value is the name of sub-directory of app/locales directory
